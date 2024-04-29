6e3a6e222ad9eea78712f84669b35032d0184abd
Jeff <38136447+fcagroupj@users.noreply.github.com>
2024-04-29 11:44:51 -0400

    Update l17_MinAbsSum.py

--- l17_MinAbsSum.py
+++ l17_MinAbsSum.py
@@ -1,6 +1,6 @@
 '''
-https://app.codility.com/demo/results/trainingM79TKG-NV8/
-[27% passed]
+https://app.codility.com/demo/results/trainingPBB3QG-3FZ/
+[90% passed]
 For a given array A of N integers and a sequence S of N integers from the set {−1, 1}, we define val(A, S) as follows:
 
 val(A, S) = |sum{ A[i]*S[i] for i = 0..N−1 }|
@@ -15,7 +15,39 @@ def solution(A)
 
 that, given an array A of N integers, computes the minimum value of val(A,S) from all possible values of val(A,S) for all possible sequences S of N integers from the set {−1, 1}.
 '''
-
+def solution(A):
+    # Implement your solution here
+    N = len(A)
+    if(N <= 0): return 0
+    A = [abs(x) for x in A]
+    sm = 0
+    mx = 0
+    for a in A:
+        sm += a
+        mx = max(mx, a)
+    counts = [0] * (mx + 1)
+    for a in A:
+        counts[a] += 1
+    Total = [-1] * (sm + 1)
+    Total[0] = 0
+    for i in range(1, len(counts)):
+        for j in range(len(Total)):
+            if(Total[j] >= 0):
+                Total[j] = counts[i]
+            elif(j - i >= 0 and Total[j-i] > 0):
+                Total[j] = Total[j-i] - 1
+            #print(1, i, j, Total)
+    # print(2, Total)
+    result = sm
+    for i in range(len(Total)//2 + 1):
+        if(Total[i] >= 0 and result > abs(sm - 2*i)):
+            result = abs(sm - 2 * i)
+    return result
+#########################################
+# as a reference
+# https://app.codility.com/demo/results/trainingM79TKG-NV8/
+# [27% passed]
+#
 def solution(A):
     # Implement your solution here
     N = len(A) 
