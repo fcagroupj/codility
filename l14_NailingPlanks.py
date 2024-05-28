ca93009755238623797afdd1015a45eeffeb4707
Jeff <38136447+fcagroupj@users.noreply.github.com>
2024-05-27 22:16:58 -0400

    Update l14_NailingPlanks.py

--- l14_NailingPlanks.py
+++ l14_NailingPlanks.py
@@ -1,6 +1,5 @@
 '''
-https://app.codility.com/demo/results/trainingPAY7KX-SBM/
-[50% passed]
+
 You are given two non-empty arrays A and B consisting of N integers. These arrays represent N planks. More precisely, A[K] is the start and B[K] the end of the K−th plank.
 
 Next, you are given a non-empty array C consisting of M integers. This array represents M nails. More precisely, C[I] is the position where you can hammer in the I−th nail.
@@ -18,6 +17,45 @@ that, given two non-empty arrays A and B consisting of N integers and a non-empt
 
 If it is not possible to nail all the planks, the function should return −1.
 '''
+##############################################
+# https://app.codility.com/demo/results/trainingXVKCYW-5SU/
+# [100%]
+# 
+def can_nail_all_planks(A, B, C, max_nails):
+    # Create an array to record the maximum nail up to each position
+    max_pos = [0] * (2 * len(C) + 1)
+    
+    for i in range(max_nails):
+        max_pos[C[i]] = 1
+    print(1, max_pos)
+    # Accumulate the prefix max to check 
+    # if any nail is available in the range
+    for i in range(1, len(max_pos)):
+        max_pos[i] += max_pos[i - 1]
+    print(2, max_pos)
+    # Check each plank
+    for i in range(len(A)):
+        if max_pos[B[i]] - max_pos[A[i] - 1] == 0:
+            return False
+    return True
+def solution(A, B, C):
+    # Implement your solution here
+    low, high = 1, len(C)
+    result = -1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if can_nail_all_planks(A, B, C, mid):
+            result = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+    
+    return result
+##############################################
+# https://app.codility.com/demo/results/trainingPAY7KX-SBM/
+# [50% passed]
+#
 def solution(A, B, C):
     # Implement your solution here
     N = len(A) 
